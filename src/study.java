425bd09fdc34846d76be3b043cb536b9be4353ae
HanSungMin99 <sungmini9876@gmail.com>
2024-12-10 17:16:15 +0900

    12/10

--- src/study.java
+++ src/study.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class study {
     public static void main(String[] args) {
@@ -173,18 +174,69 @@ public class study {
 //        //indexOf: 가장 먼저 나오는 값의 index return;
 //        System.out.println(myList.indexOf(20));
 
-        String[] arr = {"농구", "축구", "농구", "야구", "축구"};
-        // 농구: 2, 축구: 2, 야구: 1
-        Map<String, Integer> sports = new HashMap<>();
-        for (String a : arr) {
-            if (sports.containsKey(a)) {
-                sports.put(a, sports.get(a) + 1);
-            } else {
-                sports.put(a, 1);
-            }
-        }
-        System.out.println(sports);
+//        String[] arr = {"농구", "축구", "농구", "야구", "축구"};
+//        // 농구: 2, 축구: 2, 야구: 1
+//        Map<String, Integer> sports = new HashMap<>();
+//        for (String a : arr) {
+//            if (sports.containsKey(a)) {
+//                sports.put(a, sports.get(a) + 1);
+//            } else {
+//                sports.put(a, 1);
+//            }
+//        }
+//        System.out.println(sports);
 
+
+//        //Queue는 뒤에서 데이터를 넣고 앞에서 데이터를 빼는 것이 가장 최적화 되어 있다.
+//        //Queue 인터페이스를 LinkedList가 구현한 방식을 가장 많이 사용
+//        Queue<Integer> myQueue = new LinkedList<>();
+//        //LinkedList는 List와 Queue 기능을 모두 구현 가능하지만, 왼쪽에 Queue라는 인터페이스를 선언하면서 LinkedList의 사용메서드를 제한함.
+//        //중간에 값을 삽입하는 것은 성능이 저하된다.
+//        myQueue.add(10);
+//        myQueue.add(20);
+//        myQueue.add(30);
+//        //poll을 사용하면 myQueue에 데이터를 삭제하면서 가장 앞의 데이터를 반환한다
+//        System.out.println(myQueue.poll()); //poll은 무조건 맨 앞에 있는 것을 가져옴
+//        System.out.println(myQueue);
+//        //peek: myQueue의 데이터를 삭제하지는 않고, 가장 앞의 데이터를 반환
+//        System.out.println(myQueue.peek());
+//        System.out.println(myQueue);
+
+//        //Queue
+//        Queue<String> myQue = new LinkedList<>();
+//        myQue.add("문서1");
+//        myQue.add("문서2");
+//        myQue.add("문서3");
+//        while (!myQue.isEmpty()){
+//            System.out.println(myQue.poll());
+//        }
+//        //길이제한 큐: ArrayBlockingQueue
+//        Queue<String> blockingQueue = new ArrayBlockingQueue<>(3);
+//        blockingQueue.add("문서1");
+//        blockingQueue.add("문서2");
+//        blockingQueue.add("문서3");
+//        //blockingQueue.add("문서4"); //길이 제한 때문에 에러가 발생한다
+//        //offer: Queue에 여유공간이 있을 때만 add
+//        blockingQueue.offer("문서1");
+//        blockingQueue.offer("문서2");
+//        blockingQueue.offer("문서3");
+//        blockingQueue.offer("문서4"); // 길이 제한이 있어도 에러가 나지 않고, 대신 삽입도 되지 않는다.
+//        System.out.println(blockingQueue);
+
+//        //우선순위 Queue: 데이터를 꺼낼 때 정렬된 데이터 poll
+//        //전체 정렬이 아닌, poll할 때마다 최소값을 보장
+//        //최소값을 찾을 때 성능이 굉장히 좋다.
+//        Queue<Integer> pq = new PriorityQueue<>();
+//        pq.add(30);
+//        pq.add(20);
+//        pq.add(10);
+//        pq.add(40);
+//        pq.add(50);
+//        System.out.println(pq); //이렇게 조회만 하면 정렬이 되어있지 않음
+//        System.out.println(pq.poll()); //그러나 값을 꺼낼 때는 오름차순 정렬된 형태로 데이터를 꺼낸다.
+//        while(!pq.isEmpty()){
+//            System.out.println(pq.poll()); //하나꺼낼때마다 log n의 복잡도를 가짐
+//        }//Empty가 아닐 때까지 계속 값을 꺼냄
 
     }
 }
